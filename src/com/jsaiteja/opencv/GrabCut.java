1b4e2c961c8dd7dad03d95e87305aea590838db2
Sai Teja Jammalamadaka <tsjamm@gmail.com>
2014-02-22 21:19:10 +0530

    temporary commit

--- src/com/jsaiteja/opencv/GrabCut.java
+++ src/com/jsaiteja/opencv/GrabCut.java
@@ -19,7 +19,7 @@ import com.jsaiteja.utils.ImageWindow;
  */
 public class GrabCut {
 	
-	public void getForeground(String in, String out, int x, int y, int w, int h)
+	public void getForeground(String in, String out, int x, int y, int w, int h, boolean writeToFile)
 	{
 		Mat image = Highgui.imread(in);
 		ImageWindow.imshow("Input Image", in);
@@ -37,11 +37,12 @@ public class GrabCut {
 		
 		Mat output = new Mat(image.size(),CvType.CV_8UC3, new Scalar(255,255,255));
 		image.copyTo(output, result);
-		Highgui.imwrite(out, output);
+		if(writeToFile)
+			Highgui.imwrite(out, output);
 		ImageWindow.imshow("Output Image", out);
 	
 	}
-	public void getForeground(String in, String out)
+	public void getForeground(String in, String out, boolean writeToFile)
 	{
 		Mat image = Highgui.imread(in);
 		int x = 10;
@@ -49,7 +50,42 @@ public class GrabCut {
 		int w = image.cols() - (x+10);
 		int h = image.rows() - (y+10);
 		
-		getForeground(in,out,x,y,w,h);
+		getForeground(in,out,x,y,w,h,writeToFile);
+		
+	}
+	
+	public void getBackground(String in, String out, int x, int y, int w, int h, boolean writeToFile)
+	{
+		Mat image = Highgui.imread(in);
+		ImageWindow.imshow("Input Image", in);
+		
+		Rect border = new Rect(x,y,w,h);
+		
+		Mat result = new Mat();
+		Mat bgModel = new Mat();
+		Mat fgModel = new Mat();
+		Mat masker = new Mat(1, 1, CvType.CV_8U, new Scalar(Imgproc.GC_PR_BGD));
+		
+		Imgproc.grabCut(image, result, border, bgModel, fgModel, 2, Imgproc.GC_INIT_WITH_RECT);
+		
+		Core.compare(result, masker, result, Core.CMP_EQ);
+		
+		Mat output = new Mat(image.size(),CvType.CV_8UC3, new Scalar(255,255,255));
+		image.copyTo(output, result);
+		if(writeToFile)
+			Highgui.imwrite(out, output);
+		ImageWindow.imshow("Output Image", out);
+	
+	}
+	public void getBackground(String in, String out, boolean writeToFile)
+	{
+		Mat image = Highgui.imread(in);
+		int x = 10;
+		int y = 10;
+		int w = image.cols() - (x+10);
+		int h = image.rows() - (y+10);
+		
+		getBackground(in,out,x,y,w,h,writeToFile);
 		
 	}
 	
@@ -57,7 +93,8 @@ public class GrabCut {
 	{
 		System.out.println("Starting GrabCut");
 		GrabCut gc = new GrabCut();
-		gc.getForeground(in, out);
+		//gc.getForeground(in, out, true);
+		gc.getBackground(in, out, true);
 		System.out.println("Completed GrabCut");
 	}
 }
